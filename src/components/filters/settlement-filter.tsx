b48ac0c060c72d607d46399e0c02fd2aa3bcc24d
tema-skakun <frederico_f@mail.ru>
2025-06-29 00:10:50 +0300

    fix: fixes background of input options

--- src/components/filters/settlement-filter.tsx
+++ src/components/filters/settlement-filter.tsx
@@ -25,7 +25,9 @@ export const SettlementFilter = ({
 					placeholder="Название населённого пункта…"
 				/>
 				<Combobox.Options
-					className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+					className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+					style={{backgroundColor: 'rgba(255, 255, 255, 0.9)'}}
+				>
 					{(query ? fuse.search(query).map((r) => r.item) : settlements).map((item) => (
 						<Combobox.Option
 							key={item}

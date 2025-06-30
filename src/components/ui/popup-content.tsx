b7962c077a3765e81ed23c7f8864e0c2c09424e7
tema-skakun <frederico_f@mail.ru>
2025-06-30 21:14:04 +0300

    feat: delete null values from popup

--- src/components/ui/popup-content.tsx
+++ src/components/ui/popup-content.tsx
@@ -2,11 +2,13 @@ import {Feature} from '../../types/map.types';
 
 export const PopupContent = ({feature}: { feature: Feature }) => (
 	<div className="text-sm space-y-1">
-		{Object.entries(feature.properties).map(([key, value]) => (
-			<div key={key}>
-				<span className="font-medium">{key}: </span>
-				<span>{String(value)}</span>
-			</div>
-		))}
+		{Object.entries(feature.properties).map(([key, value]) => {
+			return (value && !key.includes('ID') &&
+        <div key={key}>
+          <span className="font-medium">{key}: </span>
+          <span>{String(value)}</span>
+        </div>
+			)
+		})}
 	</div>
 );
